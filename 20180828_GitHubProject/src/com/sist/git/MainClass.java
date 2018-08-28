4ea096ee598bc18dd1b0ae0c064482e7346bb53c
user30 <user30@SIST64>
2018-08-28 15:28:47 +0900

    hello

--- 20180828_GitHubProject/src/com/sist/git/MainClass.java
+++ 20180828_GitHubProject/src/com/sist/git/MainClass.java
@@ -5,6 +5,10 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello GitHub");
+		System.out.println("Hello GitHub");
+		System.out.println("Hello GitHub");
+		System.out.println("Hello GitHub");
+		System.out.println("Hello GitHub");
 	}
 
 }
